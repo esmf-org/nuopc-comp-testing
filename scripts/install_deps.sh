8642bb024a067bf7804bcbf5c4a86c69de6045d9
uturuncoglu <turuncu@ucar.edu>
2023-01-09 11:52:44 -0700

    update action yaml

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -7,6 +7,7 @@ do
         d) deps=${OPTARG};;
         i) install_dir=${OPTARG};;
         r) run_dir=${OPTARG};;
+        a) arch=${OPTARG};;
     esac
 done
 
@@ -15,6 +16,7 @@ if [ -z "$deps" ]; then
   echo "Dependencies are not given! Exiting ..."
   exit
 fi
+
 if [ ! -z `echo $deps | grep '^-'` ]; then
   echo "argument -d is given but dependencies are not listed!"
   exit
@@ -22,8 +24,13 @@ fi
 if [[ -z "$install_dir" || ! -z `echo $install_dir | grep '^-'` ]]; then
   install_dir="$HOME/.spack-ci"
 fi
+
 if [[ -z "$run_dir" || ! -z `echo $run_dir | grep '^-'` ]]; then
   run_dir=`pwd`
+fi
+
+if [[ -z "$arch" || ! -z `echo $arch | grep '^-'` ]]; then
+  arch="x86_64_v4"
 fi
 
 # print out arguments
@@ -35,9 +42,12 @@ echo "Run Directory: $run_dir";
 cd $run_dir
 
 # checkout spack
+echo "::group::Checkout Spack"
 git clone -b jcsda_emc_spack_stack https://github.com/NOAA-EMC/spack.git
+echo "::endgroup::"
 
 # create spack.yaml
+echo "::group::Create spack.yaml"
 echo "spack:" > spack.yaml
 echo "  concretizer:" >> spack.yaml
 echo "    targets:" >> spack.yaml
@@ -48,13 +58,13 @@ echo "  specs:" >> spack.yaml
 IFS=', ' read -r -a array <<< "$deps"
 for d in "${array[@]}"
 do
-  echo "  - $d target=x86_64_v4" >> spack.yaml
+  echo "  - $d target=$arch" >> spack.yaml
 done
 echo "  packages:" >> spack.yaml
 echo "    all:" >> spack.yaml
 # following is required to build same optimized spack for different github action runners
 # spack arch --known-targets command can be used to list known targets
-echo "      target: ['x86_64_v4']" >> spack.yaml
+echo "      target: ['$arch']" >> spack.yaml
 # following fixes compiler version
 echo "      compiler: [gcc@11.3.0]" >> spack.yaml
 echo "  view: $install_dir/view" >> spack.yaml
@@ -64,10 +74,19 @@ echo "    misc_cache: $install_dir/.spack-ci/misc_cache" >> spack.yaml
 echo "    test_cache: $install_dir/test_cache" >> spack.yaml
 echo "    install_tree:" >> spack.yaml
 echo "      root: $install_dir/opt" >> spack.yaml
+echo "::endgroup::"
 
 # concretize spack environment
+echo "::group::Concretize Spack Environment Using YAML Specification"
 . spack/share/spack/setup-env.sh
 spack --color always -e $run_dir/. concretize -f
+echo "::endgroup::"
 
 # install spack environment
+echo "::group::Install Spack Packages"
 spack --color always -e $run_dir/. install -j3 --deprecated --no-checksum
+echo "::endgroup::"
+
+# return output
+#echo "::set-output name=version::spack-$arch-`"
+#spack-${{ steps.build-env.outputs.SPACK_ARCH }}-${{ hashFiles('**/spack.lock') }}
