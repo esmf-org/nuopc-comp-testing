7c4217bccb8960fbaa075f76a176a960d3a2e846
uturuncoglu <turuncu@ucar.edu>
2023-01-12 16:30:55 -0700

    update action

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -21,5 +21,6 @@ cd $run_dir
 
 # install spack environment
 echo "::group::Install Spack Packages"
+. spack/share/spack/setup-env.sh
 spack --color always -e $run_dir/. install -j3 --deprecated --no-checksum
 echo "::endgroup::"
